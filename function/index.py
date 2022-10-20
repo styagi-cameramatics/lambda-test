a540c386caf891f69deb8daf78a6cedba1d829d0
styagi-cameramatics <styagi@cameramatics.com>
2022-10-20 13:18:24 +0100

    cloudformation lambda func and role

--- function/index.py
+++ function/index.py
@@ -5,7 +5,7 @@ import boto3
 s3_client = boto3.client('s3')
 
 
-def lambda_handler(event, context):
+def func_handler(event, context):
     # TODO implement
     response = s3_client.list_buckets()
 
