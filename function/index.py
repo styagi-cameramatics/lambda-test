f22f8c38f89660853beb7d2e262e70050d32651d
styagi-cameramatics <styagi@cameramatics.com>
2022-10-27 13:07:47 +0100

    lambda test

--- function/index.py
+++ function/index.py
@@ -8,8 +8,9 @@ s3_client = boto3.client('s3')
 def func_handler(event, context):
     # TODO implement
     response = s3_client.list_buckets()
+    print(response)
 
     return {
         'statusCode': 200,
-        'body': response
+        'body': "ok"
     }
